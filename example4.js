ea916fdde45045e68f3427bf2bc74e6d53ed92b3
Devadarsh4 <devadarsh7070@gmail.com>
2026-01-21 13:57:48 +0530

    example3.js

---
+++ example4.js
@@ -0,0 +1,13 @@
+let x = 2;
+let y = 5;
+
+console.log(y + x);
+console.log(y - x);
+console.log(y * x);
+console.log(y % x);
+console.log(y / x);
+console.log(y ** x);
+x += 4;
+// x = x+4;
+
+// console.log(x - y);
