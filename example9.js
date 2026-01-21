7820df12ef079034967846b07a02a7db9b0cfb95
Devadarsh4 <devadarsh7070@gmail.com>
2026-01-21 15:53:23 +0530

    commit

---
+++ example9.js
@@ -0,0 +1,16 @@
+let x = 100;
+switch (x) {
+    case 10:
+        console.log("value is 10");
+        break;
+
+
+    case 20:
+        console.log("value is 20");
+        break;
+
+    default:
+        console.log("value not match")
+
+
+}
