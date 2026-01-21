73adf2da14cd1c44e80255c40edb885780a682c6
Devadarsh4 <devadarsh7070@gmail.com>
2026-01-21 15:13:47 +0530

    example3.js

---
+++ example5.js
@@ -0,0 +1,15 @@
+let x = 10;
+let y = "10";
+console.log(x === y);
+//strict comprian;bboth value type
+console.log(x != y);
+//coercian
+console.log(x !== y);
+
+
+
+
+
+
+console.log(x == y);
+//coercian
